253da8b49795dceb28b558da02f7df6f250054ca
invincibleAntares <kumarabhishek2928@gmail.com>
2024-12-24 17:09:09 +0530

    added the DashBoard Header

--- src/app/dashboard/_components/DashboardHeader.tsx
+++ src/app/dashboard/_components/DashboardHeader.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { UserButton } from '@clerk/nextjs';
+import React from 'react';
 
 function DashboardHeader() {
   return (
-    <div>DashboardHeader</div>
-  )
+    <div className="p-5 shadow-md flex items-center justify-between bg-white ">
+      <div className="flex-1 flex justify-center">
+        <div className="text-lg font-semibold text-gray-700">
+          Welcome
+        </div>
+      </div>
+      <UserButton />
+    </div>
+  );
 }
 
-export default DashboardHeader
+export default DashboardHeader;
